00ff94f7cf3279755b13cef57869c2f61923af1e
gstraube <gstraube@mailbox.org>
2016-10-09 17:11:09 +0200

    Enable drawing

--- chip8.c
+++ chip8.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <math.h>
+
+#define WIDTH 64
+#define HEIGHT 32
 
 uint8_t memory[4096];
 uint16_t opcode;
@@ -16,6 +20,8 @@ uint8_t stack_pointer;
 
 uint16_t stack[16];
 
+uint8_t display[WIDTH][HEIGHT] = {0};
+
 int8_t load_file(char *file_name)
 {
 	FILE *input = fopen(file_name, "r");
@@ -30,6 +36,46 @@ int8_t load_file(char *file_name)
 	return 0;
 }
 
+void print_display()
+{
+	for (uint8_t i = 0; i < HEIGHT; i++) {
+		for (uint8_t j = 0; j < WIDTH; j++) {
+			if (display[j][i]) {
+				printf("*");
+			} else {
+				printf("+");
+			}
+		}
+		printf("\n");
+	}
+
+}
+
+void draw(uint16_t argument)
+{
+	uint8_t num_bytes = argument & 0xF;
+	uint8_t y_offset = v_registers[(argument >> 4) & 0xF];
+	uint8_t x_offset = v_registers[(argument >> 8) & 0xF];
+
+	uint8_t *sprite = calloc(num_bytes, sizeof(uint8_t));
+
+	for (uint8_t i = 0; i < num_bytes; i++) {
+		sprite[i] = memory[i_register + i];
+	}
+
+	for (int i = 0; i < num_bytes; i++) {
+		for (int j = 0; j < 8; j++) {
+			uint8_t x_pos = (i + x_offset) % WIDTH;
+			uint8_t y_pos = (j + y_offset) % HEIGHT;
+
+			uint8_t relevant_bit = sprite[i] << j;
+			uint8_t value = relevant_bit >> (7 - j);
+
+			display[x_pos][y_pos] ^= value; 
+		}
+	}
+}
+
 int8_t run_emulation()
 {
 	for (;;) {
@@ -37,16 +83,42 @@ int8_t run_emulation()
 		instruction = (memory[program_counter] << 8) | memory[program_counter + 1];
 
 		uint8_t op_code = instruction >> 12;
-		uint16_t argument = instruction & 8191;
+		uint16_t argument = instruction & 0xFFF;
+		uint8_t reg_number;
+		uint8_t value;
 		switch (op_code) {
 			case 0xa:
 				i_register = argument;
 				program_counter = program_counter + 2;
 				break;
 			case 0x2:
-				stack[stack_pointer] = program_counter; 
 				stack_pointer++;
+				stack[stack_pointer] = program_counter + 2; 
 				program_counter = argument;
+				break;
+			case 0x6:
+				reg_number = argument >> 8;
+				value = argument & 0xFF;
+				v_registers[reg_number] = value;
+				program_counter = program_counter + 2;
+				break;
+			case 0x7:
+				reg_number = argument >> 8;
+				value = argument & 0xFF;
+				v_registers[reg_number] += value;
+				program_counter = program_counter + 2;
+				break;
+			case 0xD:
+				draw(argument);
+				print_display();
+
+				program_counter = program_counter + 2;
+				break;
+			case 0x0:
+				if (argument == 0x0EE) {
+					program_counter = stack[stack_pointer];
+					stack_pointer--;
+				}
 				break;
 			default:
 				printf("Encountered unknown op_code %x with argument %x\n", op_code, argument);
